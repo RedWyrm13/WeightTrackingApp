d7954694e7ac4e3ff4fac7f15ef1185c912e9ac6
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2025-01-06 09:20:33 -0500

    Remove DayViewModelFactory Logic.

--- app/src/main/java/com/example/weighttracking/ui/theme/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/example/weighttracking/ui/theme/viewmodel/ViewModelFactory.kt
@@ -1,27 +1,8 @@
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
-import com.example.weighttracking.data.CalendarDate
 import com.example.weighttracking.data.CalendarRepositoryImplementation
 import com.example.weighttracking.ui.theme.viewmodel.CalendarViewModel
-import com.example.weighttracking.ui.theme.viewmodel.DayViewModel
-
-class DayViewModelFactory(
-    private val repository: CalendarRepositoryImplementation,
-    private val calendarDay: CalendarDate
-) : ViewModelProvider.Factory {
-
-    @Suppress("UNCHECKED_CAST")
-
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-
-        if (modelClass.isAssignableFrom(DayViewModel::class.java)) {
-            return DayViewModel(repository, calendarDay) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
-}
 
 
 class CalendarViewModelFactory(
@@ -34,14 +15,4 @@ class CalendarViewModelFactory(
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
-}
-
-fun createDayViewModelForDay(
-    repository: CalendarRepositoryImplementation,
-    owner: ViewModelStoreOwner,
-    calendarDay: CalendarDate
-): DayViewModel {
-    val factory = DayViewModelFactory(repository, calendarDay)
-    return ViewModelProvider(owner, factory)
-        .get("DayViewModel_${calendarDay.date}", DayViewModel::class.java)
 }
