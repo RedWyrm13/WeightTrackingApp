c8e58a5994236bb492e3a14318cd06ea054f70c0
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2025-01-06 11:51:32 -0500

    Added Button to save weight

--- app/src/main/java/com/example/weighttracking/ui/theme/WeightTrackingApp.kt
+++ app/src/main/java/com/example/weighttracking/ui/theme/WeightTrackingApp.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
@@ -220,6 +221,9 @@ fun ButtonBox(screenViewModel: ScreenViewModel) {
             label = { Text("Weight") }, // Label shown above the text field
             placeholder = { Text("Enter weight") }, // Hint text
         )
+        Button(onClick = { screenViewModel.updateWeight(screenViewModel.weightInput.toDouble()) }) {
+            Text("Save Weight")
+        }
     }
 }
 
