aaaef40e93fe89d3c1b4623de747ee3f5c7302f8
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2025-01-08 15:08:20 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/weighttracking/ui/theme/screens/StartScreen.kt
@@ -0,0 +1,9 @@
+package com.example.weighttracking.ui.theme.screens
+
+import androidx.compose.runtime.Composable
+import com.example.weighttracking.ui.theme.viewmodel.ScreenViewModel
+
+@Composable
+fun StartScreen(userViewModel: UserViewModel){
+
+}
