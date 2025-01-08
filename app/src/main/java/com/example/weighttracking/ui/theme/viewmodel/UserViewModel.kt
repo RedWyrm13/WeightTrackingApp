5d6f985d35cde8110eafb62128407ed0cbe5a1e5
RedWyrm13 <Andrewmaciejunes@yahoo.com>
2025-01-08 17:30:41 -0500

    Added data logic for start screen

--- app/src/main/java/com/example/weighttracking/ui/theme/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/weighttracking/ui/theme/viewmodel/UserViewModel.kt
@@ -1,6 +1,19 @@
 package com.example.weighttracking.ui.theme.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.weighttracking.data.CalendarRepositoryImplementation
 
-class UserViewModel: ViewModel() {
+class UserViewModel(private val calendarRepositoryImplementation: CalendarRepositoryImplementation): ViewModel() {
+    var currentWeightInput by mutableStateOf("")
+
+    // This is used to determine if we use kg or lbs. True is lbs.  False is kg
+    var isPounds: Boolean by mutableStateOf(true)
+
+    val currentWeihgt: Double
+        get() = currentWeightInput.toDoubleOrNull() ?: 0.0
+
+
 }
